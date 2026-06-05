4512d92715ef4b962dbd2dfc2886af464c51ea35
MarcoTulioGT <devopsgt@gmail.com>
2026-06-04 18:33:35 -0600

    de nuevo

--- config.sh
+++ config.sh
@@ -1,10 +1,3 @@
-export SERVICE_ACCOUNT="[EMAIL_ADDRESS]"
-export REPO="mcatalangt/iac_kubespray"
-
-gcloud iam service-accounts add-iam-policy-binding "${SERVICE_ACCOUNT}" \
-  --project="${PROJECT_ID}" \
-  --role="roles/iam.workloadIdentityUser" \
-  --member="principalSet://iam.googleapis.com/projects/${PROJECT_NUMBER}/locations/global/workloadIdentityPools/github-actions-pool/attribute.repository/${REPO}"
 
 
 
@@ -12,3 +5,13 @@ gcloud iam service-accounts add-iam-policy-binding "${SERVICE_ACCOUNT}" \
   --project="lakehouse-475923" \
   --role="roles/iam.workloadIdentityUser" \
   --member="principalSet://iam.googleapis.com/projects/397005106927/locations/global/workloadIdentityPools/github-actions-pool/attribute.repository/mcatalangt/iac_kubespray"
+
+
+
+  gcloud projects add-iam-policy-binding lakehouse-475923 \
+  --member="serviceAccount:github-actions-sa@lakehouse-475923.iam.gserviceaccount.com" \
+  --role="roles/compute.securityAdmin"
+
+gcloud projects add-iam-policy-binding lakehouse-475923 \
+  --member="serviceAccount:github-actions-sa@lakehouse-475923.iam.gserviceaccount.com" \
+  --role="roles/compute.networkAdmin"
